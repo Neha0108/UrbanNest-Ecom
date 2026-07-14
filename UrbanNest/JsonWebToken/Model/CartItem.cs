351eaf9bf18a0cd2cfd1dd3d1629832740b97499
Neha Lamba <Neha.lamba@evalueserve.com>
2026-07-14 14:59:07 +0530

    feat: implement cart and wishlist functionality

---
+++ UrbanNest/JsonWebToken/Model/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using UrbanNest.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace UrbanNest.Model
+{
+    public class CartItem
+    {
+        [Key]
+        public int CartItemId { get; set; }
+        public int CartId { get; set; }
+        public Cart Cart { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
