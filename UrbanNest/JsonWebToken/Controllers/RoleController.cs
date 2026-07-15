e2d0c95d100d4999d1d7d7af985e703961795f46
Neha Lamba <Neha.lamba@evalueserve.com>
2026-07-15 11:15:09 +0530

    admincontroller, consumercontroller and rolecontroller

---
+++ UrbanNest/JsonWebToken/Controllers/RoleController.cs
@@ -0,0 +1,47 @@
+﻿using UrbanNest.Repository;
+using Microsoft.AspNetCore.Mvc;
+using UrbanNest.DTO;
+using Microsoft.AspNetCore.Authorization;
+
+namespace UrbanNest.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRole role;
+        public RoleController(IRole role)
+        {
+            this.role = role;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> addRole([FromBody] RoleDTO roles)
+        {
+            var data = await role.add(roles);
+            return Ok(data);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getAllRoles()
+        {
+            var data = await role.getAll();
+            return Ok(data);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> deleteRole(int id)
+        {
+            var message = await role.delete(id);
+            return Ok(message);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> deleteRoleUser(int id)
+        {
+            var message = await role.deleteRoleUser(id);
+            return Ok(message);
+        }
+    }
+}
